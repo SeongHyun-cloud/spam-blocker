d91fb3170fb84922080c16e7f75dbdd72a1ef791
SeongHyun-cloud <shparkwk@gmail.com>
2022-06-19 22:55:13 +0900

    Initial Commit

---
+++ spam_blocker/spam_blockerApp.swift
@@ -0,0 +1,17 @@
+//
+//  spam_blockerApp.swift
+//  spam_blocker
+//
+//  Created by Seonghyun  Park on 6/19/22.
+//
+
+import SwiftUI
+
+@main
+struct spam_blockerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
